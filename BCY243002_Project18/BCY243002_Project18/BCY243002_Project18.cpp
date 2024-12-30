31eecc77fa21e5b323b41fcb2bb99ef9584eaa78
ridarauf-2 <Rida.12.2005@gmail.com>
2024-12-30 19:09:27 +0500

    Add files via upload

---
+++ BCY243002_Project18/BCY243002_Project18/BCY243002_Project18.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	cout << "my name is:Rida Rauf,Reg No:BCY243002" << endl;
+	cout << "enter a number: ";
+	cin >> n;
+	for (int i = n;i > 0;i--) {
+		cout << i << " ";
+	}
+	return 0;
+}
