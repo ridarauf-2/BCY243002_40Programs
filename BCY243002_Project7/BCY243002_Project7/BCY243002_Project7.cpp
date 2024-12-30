607097f9c5aeeff7f306a5fd3dbf1ac685705294
ridarauf-2 <Rida.12.2005@gmail.com>
2024-12-30 18:58:16 +0500

    Add files via upload

---
+++ BCY243002_Project7/BCY243002_Project7/BCY243002_Project7.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int Num;
+	cout << "my name is:Rida Rauf,Reg No:BCY243002" << endl;
+	cout << "Enter a number: ";
+	cin >> Num;
+	if (Num % 2 == 0) {
+		cout << "number is even";
+	}
+	else {
+		cout << "number is odd";
+	}
+	return 0;
+}
